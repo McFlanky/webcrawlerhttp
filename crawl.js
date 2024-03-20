c416242d76e47c759b137bfc203e14f1c24a4f83
McFlanky <cturjanica02@gmail.com>
2024-03-20 10:55:00 -0400

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,12 +1,39 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+        // relative url
+      try {
+        urls.push(new URL(linkElement.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${linkElement.href}`);
+      }
+        // absolute url
+    } else {
+      try {
+        urls.push(new URL(linkElement.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${linkElement.href}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
-        return hostPath.slice(0, -1)
-    } 
-    return hostPath
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
 }
 
 module.exports = {
-  normalizeURL
-}
+  normalizeURL,
+  getURLsFromHTML,
+};
